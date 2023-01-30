69049adebfb467a4cb899f0baf03684087725e47
Farshad Fahimi <FarshadFahimi5@Gmail.com>
2023-01-30 12:58:45 +0330

    Implementing Seed data for product and product category

---
+++ src/WebApi/Data/AppDbContextInitialiser.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Data.Entities;
+using WebApi.Data.Enums;
+
+namespace WebApi.Data;
+
+public class AppDbContextInitialiser
+{
+    private readonly ILogger<AppDbContextInitialiser> _logger;
+    private readonly AppDbContext _context;
+
+    public AppDbContextInitialiser(ILogger<AppDbContextInitialiser> logger, AppDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public async Task InitialiseAsync()
+    {
+        try
+        {
+            if (_context.Database.IsSqlServer())
+            {
+                await _context.Database.MigrateAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while initialising the database.");
+            throw;
+        }
+    }
+
+    public async Task SeedAsync()
+    {
+        try
+        {
+            await TrySeedAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while seeding the database.");
+            throw;
+        }
+    }
+
+    public async Task TrySeedAsync()
+    {
+        if (!_context.ProductCategories.Any())
+        {
+            await _context.ProductCategories.AddRangeAsync(ProductCategories);
+            await _context.SaveChangesAsync();
+        }
+        
+        if (!_context.Products.Any())
+        {
+            var mobileCategoryId = await _context.ProductCategories
+                .Where(p=>p.Title == "Mobile")
+                .Select(p=>p.Id)
+                .FirstAsync();
+
+            var products = Products();
+            foreach (var item in products)
+            {
+                item.CategoryId = mobileCategoryId;
+            }
+
+            await _context.Products.AddRangeAsync(products);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public IList<ProductCategory> ProductCategories => new List<ProductCategory>()
+    {
+        new ProductCategory()
+        {
+            Title= "Mobile",
+        },
+        new ProductCategory()
+        {
+            Title= "Clothes",
+        },
+    };
+
+    public List<Product> Products() => new List<Product>()
+    {
+        new Product()
+        {
+            Name = "Samsung A10 Silver",
+            Price = decimal.Parse("10550.382"),
+            Status = EProductStatus.SoldOut,
+            Description = "Description of Samsung A10 Silver"
+        },
+        new Product()
+        {
+            Name = "Samsung A70 gold",
+            Price = decimal.Parse("22050.382"),
+            Status = EProductStatus.SoldOut,
+            Description = "Description of Samsung A70 gold"
+        },
+        new Product()
+        {
+            Name = "Samsung A12 Silver",
+            Price = decimal.Parse("9550.382"),
+            Status = EProductStatus.Available,
+            Description = "Description of Samsung A12 Silver"
+        },
+        new Product()
+        {
+            Name = "Samsung A32",
+            Price = decimal.Parse("16550.382"),
+            Status = EProductStatus.Available,
+            Description = "Description of Samsung A32"
+        },
+        new Product()
+        {
+            Name = "Samsung A30s",
+            Price = decimal.Parse("1050.382"),
+            Status = EProductStatus.SoldOut,
+            Description = "Description of Samsung A30s"
+        },
+    };
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -17,14 +17,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(p => p.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConncetion")));
 
 builder.Services.AddScoped<IAppDbContext>
     (p => p.GetRequiredService<AppDbContext>());
+builder.Services.AddScoped<AppDbContextInitialiser>();
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<AppSchema>();
-//builder.Services.AddScoped<ProductQuery>();
 builder.Services.AddSingleton<IDataLoaderContextAccessor, DataLoaderContextAccessor>();
 builder.Services.AddSingleton<DataLoaderDocumentListener>();
 
@@ -35,18 +36,28 @@ builder.Services.AddGraphQL(p => p
     .AddUserContextBuilder(context=> new MyGraphQLUserContext(context.User))
 );
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    // Initialise and seed database
+    using var scope = app.Services.CreateScope();
+    var initialiser = scope
+        .ServiceProvider
+        .GetRequiredService<AppDbContextInitialiser>();
+    await initialiser.InitialiseAsync();
+    await initialiser.SeedAsync();
 }
+app.UseCors(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 app.UseGraphQL<AppSchema>(); 
 app.UseGraphQLPlayground();
